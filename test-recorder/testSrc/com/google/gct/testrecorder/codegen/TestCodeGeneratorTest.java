0f4bd0337651bccaba49eec5b1c4c60bad9b1914
Stas Negara <snegara@google.com>
2017-01-20 15:49:37 -0800

    Add Robo bootstrap script generation and saving.
    A user can record a sequence of actions using Test Recorder
    and then save it as a Robo bootstrap script in JSON format
    (such that it is readable and editable). The bootstrap script
    can be used to guide the Robo's app exploration, e.g., to
    reach a particular activity, to complete the login process, etc.
    
    Test: Covered by test com.google.gct.testrecorder.ui.RecordingDialogTest
    Change-Id: I37e8e3e6c7670408e651b6a7a854d648e5d353e1

--- test-recorder/testSrc/com/google/gct/testrecorder/codegen/TestCodeGeneratorTest.java
+++ test-recorder/testSrc/com/google/gct/testrecorder/codegen/TestCodeGeneratorTest.java
@@ -15,10 +15,6 @@
  */
 package com.google.gct.testrecorder.codegen;
 
-import com.google.common.collect.Lists;
-import com.google.gct.testrecorder.event.ElementDescriptor;
-import com.google.gct.testrecorder.event.TestRecorderAssertion;
-import com.google.gct.testrecorder.event.TestRecorderEvent;
 import com.google.gct.testrecorder.util.ResourceHelper;
 import com.intellij.codeInsight.actions.OptimizeImportsProcessor;
 import com.intellij.codeInsight.actions.ReformatCodeProcessor;
@@ -34,18 +30,15 @@ import org.apache.commons.io.FileUtils;
 import org.jetbrains.android.AndroidTestCase;
 
 import java.io.File;
-import java.util.List;
 
-import static com.google.gct.testrecorder.event.TestRecorderAssertion.EXISTS;
-import static com.google.gct.testrecorder.event.TestRecorderAssertion.TEXT_IS;
-import static com.google.gct.testrecorder.event.TestRecorderEvent.SwipeDirection.Right;
+import static com.google.gct.testrecorder.util.EventsCreator.createEvents;
 
 public class TestCodeGeneratorTest extends AndroidTestCase {
 
   public void testCodeGeneration() throws Exception {
     PsiClass testClass = createTestClass();
 
-    TestCodeGenerator testCodeGenerator = new TestCodeGenerator(myFacet, testClass, createEvents(), "p1.p2.MyActivity", false, false);
+    TestCodeGenerator testCodeGenerator = new TestCodeGenerator(myFacet, testClass, createEvents(System.currentTimeMillis()), "p1.p2.MyActivity", false, false);
 
     String testFilePath = testClass.getContainingFile().getVirtualFile().getPath();
     VirtualFile testVirtualFile = LocalFileSystem.getInstance().findFileByPath(testFilePath);
@@ -94,85 +87,6 @@ public class TestCodeGeneratorTest extends AndroidTestCase {
     } catch (Exception e) {
       throw new RuntimeException("Failed to read the expected test class content " + expectedTestClass.getAbsolutePath(), e);
     }
-  }
-
-  private List<Object> createEvents() {
-    List<Object> events = Lists.newLinkedList();
-
-    TestRecorderEvent viewClickEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, System.currentTimeMillis());
-    viewClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 0, "resourceId1", "content description 1", ""));
-    viewClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 1, "parentResourceId1", "", ""));
-    viewClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId2", "parent content description 1", ""));
-    events.add(viewClickEvent);
-
-    TestRecorderEvent viewClickEvent2 = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, System.currentTimeMillis());
-    viewClickEvent2.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "", "", ""));
-    viewClickEvent2.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "", "", ""));
-    events.add(viewClickEvent2);
-
-    TestRecorderEvent viewClickEvent3 = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, System.currentTimeMillis());
-    viewClickEvent3.addElementDescriptor(new ElementDescriptor("GridClass", -1, 0, "gridResourceId", "", ""));
-    viewClickEvent3.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "gridParentResourceId", "", ""));
-    viewClickEvent3.setRecyclerViewPosition(5);
-    events.add(viewClickEvent3);
-
-    TestRecorderAssertion assertion = new TestRecorderAssertion(EXISTS);
-    assertion.addElementDescriptor(new ElementDescriptor("ElementClass", -1, -1, "resourceId2", "content description 2", ""));
-    events.add(assertion);
-
-    TestRecorderEvent viewLongClickEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_LONG_CLICK, System.currentTimeMillis());
-    viewLongClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1,-1, "resourceId2", "content description 2", ""));
-    events.add(viewLongClickEvent);
-
-    TestRecorderEvent listItemClickEvent = new TestRecorderEvent(TestRecorderEvent.LIST_ITEM_CLICK, System.currentTimeMillis());
-    listItemClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 2, "resourceId3", "", ""));
-    listItemClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId3", "", ""));
-    listItemClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 1, "parentResourceId4", "", ""));
-    events.add(listItemClickEvent);
-
-    TestRecorderEvent pressBackEvent = new TestRecorderEvent(TestRecorderEvent.PRESS_BACK, System.currentTimeMillis());
-    events.add(pressBackEvent);
-
-    TestRecorderEvent delayedMessagePostEvent = new TestRecorderEvent(TestRecorderEvent.DELAYED_MESSAGE_POST, System.currentTimeMillis());
-    delayedMessagePostEvent.setDelayTime(1500);
-    events.add(delayedMessagePostEvent);
-
-    TestRecorderEvent textChangeEvent = new TestRecorderEvent(TestRecorderEvent.TEXT_CHANGE, System.currentTimeMillis());
-    textChangeEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "resourceId4", "content description 3", "original text"));
-    textChangeEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId5", "", ""));
-    textChangeEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId6", "parent content description 2", ""));
-    textChangeEvent.setReplacementText("replacement \n\ntext\n");
-    events.add(textChangeEvent);
-
-    TestRecorderAssertion assertion2 = new TestRecorderAssertion(TEXT_IS);
-    assertion2.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "resourceId4", "content description 3", "replacement \n\ntext\n"));
-    assertion2.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId5", "", ""));
-    assertion2.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId6", "parent content description 2", ""));
-    assertion2.setText("replacement \n\ntext\n");
-    events.add(assertion2);
-
-    TestRecorderEvent pressEditorActionEvent = new TestRecorderEvent(TestRecorderEvent.PRESS_EDITOR_ACTION, System.currentTimeMillis());
-    pressEditorActionEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "resourceId4", "content description 3", "replacement \n\ntext\n"));
-    pressEditorActionEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId5", "", ""));
-    pressEditorActionEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId6", "parent content description 2", ""));
-    events.add(pressEditorActionEvent);
-
-    TestRecorderEvent swipeEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_SWIPE, System.currentTimeMillis());
-    swipeEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 0, "resourceId5", "content description 4", ""));
-    swipeEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 1, "parentResourceId7", "", "parent text"));
-    swipeEvent.setSwipeDirection(Right);
-    events.add(swipeEvent);
-
-    TestRecorderEvent viewAdapterItemClickEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, System.currentTimeMillis());
-    viewAdapterItemClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", 3, -1, "", "", ""));
-    viewAdapterItemClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, -1, "list", "", ""));
-    events.add(viewAdapterItemClickEvent);
-
-    TestRecorderEvent trailingDelayedMessagePostEvent = new TestRecorderEvent(TestRecorderEvent.DELAYED_MESSAGE_POST, System.currentTimeMillis());
-    trailingDelayedMessagePostEvent.setDelayTime(2500);
-    events.add(trailingDelayedMessagePostEvent);
-
-    return events;
   }
 
 }
---
+++ test-recorder/testSrc/com/google/gct/testrecorder/util/EventsCreator.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.gct.testrecorder.util;
+
+import com.google.common.collect.Lists;
+import com.google.gct.testrecorder.event.ElementDescriptor;
+import com.google.gct.testrecorder.event.TestRecorderAssertion;
+import com.google.gct.testrecorder.event.TestRecorderEvent;
+
+import java.util.List;
+
+import static com.google.gct.testrecorder.event.TestRecorderAssertion.EXISTS;
+import static com.google.gct.testrecorder.event.TestRecorderAssertion.TEXT_IS;
+import static com.google.gct.testrecorder.event.TestRecorderEvent.SwipeDirection.Right;
+
+public class EventsCreator {
+
+  public static List<Object> createEvents(long timestamp) {
+    List<Object> events = Lists.newLinkedList();
+
+    TestRecorderEvent viewClickEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, timestamp);
+    viewClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 0, "resourceId1", "content description 1", ""));
+    viewClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 1, "parentResourceId1", "", ""));
+    viewClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId2", "parent content description 1", ""));
+    events.add(viewClickEvent);
+
+    TestRecorderEvent viewClickEvent2 = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, timestamp);
+    viewClickEvent2.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "", "", ""));
+    viewClickEvent2.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "", "", ""));
+    events.add(viewClickEvent2);
+
+    TestRecorderEvent viewClickEvent3 = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, timestamp);
+    viewClickEvent3.addElementDescriptor(new ElementDescriptor("GridClass", -1, 0, "gridResourceId", "", ""));
+    viewClickEvent3.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "gridParentResourceId", "", ""));
+    viewClickEvent3.setRecyclerViewPosition(5);
+    events.add(viewClickEvent3);
+
+    TestRecorderAssertion assertion = new TestRecorderAssertion(EXISTS);
+    assertion.addElementDescriptor(new ElementDescriptor("ElementClass", -1, -1, "resourceId2", "content description 2", ""));
+    events.add(assertion);
+
+    TestRecorderEvent viewLongClickEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_LONG_CLICK, timestamp);
+    viewLongClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1,-1, "resourceId2", "content description 2", ""));
+    events.add(viewLongClickEvent);
+
+    TestRecorderEvent listItemClickEvent = new TestRecorderEvent(TestRecorderEvent.LIST_ITEM_CLICK, timestamp);
+    listItemClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 2, "resourceId3", "", ""));
+    listItemClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId3", "", ""));
+    listItemClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 1, "parentResourceId4", "", ""));
+    events.add(listItemClickEvent);
+
+    TestRecorderEvent pressBackEvent = new TestRecorderEvent(TestRecorderEvent.PRESS_BACK, timestamp);
+    events.add(pressBackEvent);
+
+    TestRecorderEvent delayedMessagePostEvent = new TestRecorderEvent(TestRecorderEvent.DELAYED_MESSAGE_POST, timestamp);
+    delayedMessagePostEvent.setDelayTime(1500);
+    events.add(delayedMessagePostEvent);
+
+    TestRecorderEvent textChangeEvent = new TestRecorderEvent(TestRecorderEvent.TEXT_CHANGE, timestamp);
+    textChangeEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "resourceId4", "content description 3", "original text"));
+    textChangeEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId5", "", ""));
+    textChangeEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId6", "parent content description 2", ""));
+    textChangeEvent.setReplacementText("replacement \n\ntext\n");
+    events.add(textChangeEvent);
+
+    TestRecorderAssertion assertion2 = new TestRecorderAssertion(TEXT_IS);
+    assertion2.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "resourceId4", "content description 3", "replacement \n\ntext\n"));
+    assertion2.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId5", "", ""));
+    assertion2.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId6", "parent content description 2", ""));
+    assertion2.setText("replacement \n\ntext\n");
+    events.add(assertion2);
+
+    TestRecorderEvent pressEditorActionEvent = new TestRecorderEvent(TestRecorderEvent.PRESS_EDITOR_ACTION, timestamp);
+    pressEditorActionEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 1, "resourceId4", "content description 3", "replacement \n\ntext\n"));
+    pressEditorActionEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 0, "parentResourceId5", "", ""));
+    pressEditorActionEvent.addElementDescriptor(new ElementDescriptor("ParentClass2", -1, 2, "parentResourceId6", "parent content description 2", ""));
+    events.add(pressEditorActionEvent);
+
+    TestRecorderEvent swipeEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_SWIPE, timestamp);
+    swipeEvent.addElementDescriptor(new ElementDescriptor("ElementClass", -1, 0, "resourceId5", "content description 4", ""));
+    swipeEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, 1, "parentResourceId7", "", "parent text"));
+    swipeEvent.setSwipeDirection(Right);
+    events.add(swipeEvent);
+
+    TestRecorderEvent viewAdapterItemClickEvent = new TestRecorderEvent(TestRecorderEvent.VIEW_CLICK, timestamp);
+    viewAdapterItemClickEvent.addElementDescriptor(new ElementDescriptor("ElementClass", 3, -1, "", "", ""));
+    viewAdapterItemClickEvent.addElementDescriptor(new ElementDescriptor("ParentClass1", -1, -1, "list", "", ""));
+    events.add(viewAdapterItemClickEvent);
+
+    TestRecorderEvent trailingDelayedMessagePostEvent = new TestRecorderEvent(TestRecorderEvent.DELAYED_MESSAGE_POST, timestamp);
+    trailingDelayedMessagePostEvent.setDelayTime(2500);
+    events.add(trailingDelayedMessagePostEvent);
+
+    return events;
+  }
+
+}
