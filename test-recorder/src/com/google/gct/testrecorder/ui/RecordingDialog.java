674525273c765cf4f1de993a438f85f242a00189
Sergio Murguia <srmurguia@google.com>
2017-04-25 18:37:42 -0700

    Add information on what triggered a gradle sync
    Patch 2: add static import for triggers
    
    Patch 3: removed unused import
    
    Test: Current tests should work
    Change-Id: I43d115f57a12c7198a913ab202ef148d5a66770a

--- test-recorder/src/com/google/gct/testrecorder/ui/RecordingDialog.java
+++ test-recorder/src/com/google/gct/testrecorder/ui/RecordingDialog.java
@@ -78,6 +78,7 @@ import static com.google.gct.testrecorder.event.TestRecorderAssertion.*;
 import static com.google.gct.testrecorder.event.TestRecorderEvent.SUPPORTED_EVENTS;
 import static com.google.gct.testrecorder.util.ImageHelper.rotateImage;
 import static com.google.gct.testrecorder.util.UiAutomatorNodeHelper.*;
+import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIGGER_PROJECT_MODIFIED;
 import static org.apache.commons.lang.StringUtils.isEmpty;
 
 public class RecordingDialog extends DialogWrapper implements TestRecorderEventListener {
@@ -589,7 +590,7 @@ public class RecordingDialog extends DialogWrapper implements TestRecorderEventL
 
           myGradleBuildModel.applyChanges();
 
-          GradleSyncInvoker.getInstance().requestProjectSyncAndSourceGeneration(myProject, null);
+          GradleSyncInvoker.getInstance().requestProjectSyncAndSourceGeneration(myProject, null, TRIGGER_PROJECT_MODIFIED);
         });
       }
     }.queue();
