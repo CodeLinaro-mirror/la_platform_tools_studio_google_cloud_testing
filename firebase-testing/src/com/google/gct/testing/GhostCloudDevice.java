48124baabd161c33edfa2e4dddafc10d20b3e0d0
Joshua Duong <joshuaduong@google.com>
2017-11-01 16:17:41 -0700

    [ScreenRecord] Device API change.
    Bug: b/65589775
    
    Test: N/A
    Change-Id: Idafaae69b0e6fc0870eb47f2ec16fc5618d0101b

--- firebase-testing/src/com/google/gct/testing/GhostCloudDevice.java
+++ firebase-testing/src/com/google/gct/testing/GhostCloudDevice.java
@@ -19,7 +19,10 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.ddmlib.*;
 import com.android.ddmlib.log.LogReceiver;
+import com.android.repository.api.ProgressIndicator;
 import com.android.sdklib.AndroidVersion;
+import com.android.sdklib.repository.AndroidSdkHandler;
+import com.android.utils.ILogger;
 import com.google.api.services.testing.model.AndroidDevice;
 import com.google.api.services.testing.model.Device;
 
@@ -112,6 +115,14 @@ public class GhostCloudDevice implements IDevice {
   }
 
   @Override
+  public boolean hasEmulatorAdvancedFeature(@NonNull EmulatorFeature feature,
+                                            @NonNull AndroidSdkHandler androidSdkHandler,
+                                            @NonNull ProgressIndicator progressIndicator,
+                                            @NonNull ILogger log) {
+    return false;
+  }
+
+  @Override
   public String getMountPoint(String name) {
     return null;
   }
@@ -176,6 +187,11 @@ public class GhostCloudDevice implements IDevice {
                                   @NonNull ScreenRecorderOptions options,
                                   @NonNull IShellOutputReceiver receiver)
     throws TimeoutException, AdbCommandRejectedException, IOException, ShellCommandUnresponsiveException {
+
+  }
+
+  @Override
+  public void stopEmulatorScreenRecorder() {
 
   }
 
