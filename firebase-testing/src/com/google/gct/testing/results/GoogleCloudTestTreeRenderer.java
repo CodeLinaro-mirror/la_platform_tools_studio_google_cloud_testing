3467f28c13ebba5218ff61bc6bf7eebd8c6adf87
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2019-04-05 13:44:50 +0300

    IDEA-195753: TestTreeView must support Tree.rowHeight
    Change-Id: I58521addc65846526fc092fed556116068bbc3ff

--- firebase-testing/src/com/google/gct/testing/results/GoogleCloudTestTreeRenderer.java
+++ firebase-testing/src/com/google/gct/testing/results/GoogleCloudTestTreeRenderer.java
@@ -16,10 +16,8 @@
 package com.google.gct.testing.results;
 
 import com.intellij.execution.testframework.TestConsoleProperties;
-import com.intellij.execution.testframework.TestTreeView;
 import com.intellij.ui.ColoredTreeCellRenderer;
-import com.intellij.ui.SimpleTextAttributes;
-import com.intellij.util.ui.JBUI;
+import com.intellij.ui.RelativeFont;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
@@ -27,13 +25,19 @@ import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 
+import static com.intellij.ide.ui.UISettings.setupAntialiasing;
+import static com.intellij.ui.SimpleTextAttributes.GRAYED_ATTRIBUTES;
+import static com.intellij.util.ui.UIUtil.getTreeSelectionForeground;
+
 public class GoogleCloudTestTreeRenderer extends ColoredTreeCellRenderer {
   @NonNls private static final String SPACE_STRING = " ";
 
   private final TestConsoleProperties myConsoleProperties;
   private GoogleCloudTestingRootTestProxyFormatter myAdditionalRootFormatter;
-  private int myDurationWidth = -1;
-  private int myRow;
+  private String myDurationText;
+  private Color myDurationColor;
+  private int myDurationWidth;
+  private int myDurationOffset;
 
   public GoogleCloudTestTreeRenderer(final TestConsoleProperties consoleProperties) {
     myConsoleProperties = consoleProperties;
@@ -47,8 +51,6 @@ public class GoogleCloudTestTreeRenderer extends ColoredTreeCellRenderer {
                                     final boolean leaf,
                                     final int row,
                                     final boolean hasFocus) {
-    myRow = row;
-    myDurationWidth = -1;
     final DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
     final Object userObj = node.getUserObject();
     if (userObj instanceof GoogleCloudTestNodeDescriptor) {
@@ -70,13 +72,12 @@ public class GoogleCloudTestTreeRenderer extends ColoredTreeCellRenderer {
       }
 
       if (TestConsoleProperties.SHOW_INLINE_STATISTICS.value(myConsoleProperties)) {
-        String durationString = testProxy.getDurationString(myConsoleProperties);
-        if (durationString != null) {
-          durationString = "  " + durationString;
-          myDurationWidth = getFontMetrics(getFont()).stringWidth(durationString);
-          if (((TestTreeView)myTree).isExpandableHandlerVisibleForCurrentRow(myRow)) {
-            append(durationString);
-          }
+        myDurationText = testProxy.getDurationString(myConsoleProperties);
+        if (myDurationText != null) {
+          FontMetrics metrics = getFontMetrics(RelativeFont.SMALL.derive(getFont()));
+          myDurationWidth = metrics.stringWidth(myDurationText);
+          myDurationOffset = metrics.getHeight() / 2; // an empty area before and after the text
+          myDurationColor = selected ? getTreeSelectionForeground(hasFocus) : GRAYED_ATTRIBUTES.getFgColor();
         }
       }
       //Done
@@ -86,16 +87,15 @@ public class GoogleCloudTestTreeRenderer extends ColoredTreeCellRenderer {
     //strange node
     final String text = node.toString();
     //no icon
-    append(text != null ? text : SPACE_STRING, SimpleTextAttributes.GRAYED_ATTRIBUTES);
+    append(text != null ? text : SPACE_STRING, GRAYED_ATTRIBUTES);
   }
 
   @NotNull
   @Override
   public Dimension getPreferredSize() {
     final Dimension preferredSize = super.getPreferredSize();
-    return myDurationWidth < 0 || ((TestTreeView)myTree).isExpandableHandlerVisibleForCurrentRow(myRow)
-           ? preferredSize
-           : JBUI.size(preferredSize.width + myDurationWidth, preferredSize.height);
+    if (myDurationWidth > 0) preferredSize.width += myDurationWidth + myDurationOffset;
+    return preferredSize;
   }
 
   public TestConsoleProperties getConsoleProperties() {
@@ -108,5 +108,31 @@ public class GoogleCloudTestTreeRenderer extends ColoredTreeCellRenderer {
 
   public void removeAdditionalRootFormatter() {
     myAdditionalRootFormatter = null;
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    setupAntialiasing(g);
+    Shape clip = null;
+    int width = getWidth();
+    int height = getHeight();
+    if (isOpaque()) {
+      // paint background for expanded row
+      g.setColor(getBackground());
+      g.fillRect(0, 0, width, height);
+    }
+    if (myDurationWidth > 0) {
+      width -= myDurationWidth + myDurationOffset;
+      if (width > 0 && height > 0) {
+        g.setColor(myDurationColor);
+        g.setFont(RelativeFont.SMALL.derive(getFont()));
+        g.drawString(myDurationText, width + myDurationOffset / 2, getTextBaseLine(g.getFontMetrics(), height));
+        clip = g.getClip();
+        g.clipRect(0, 0, width, height);
+      }
+    }
+    super.paintComponent(g);
+    // restore clip area if needed
+    if (clip != null) g.setClip(clip);
   }
 }
