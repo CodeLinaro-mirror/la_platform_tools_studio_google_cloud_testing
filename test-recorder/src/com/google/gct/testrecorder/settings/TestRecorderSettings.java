4d563f52f26b31bbc7974c9557b41a6d2c5398de
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-03-26 22:28:25 +0100

    deprecate ServiceManager.getService()
    Change-Id: Ide790135ddc2a0c79244f57e28e3e715b07d7b55

--- test-recorder/src/com/google/gct/testrecorder/settings/TestRecorderSettings.java
+++ test-recorder/src/com/google/gct/testrecorder/settings/TestRecorderSettings.java
@@ -15,8 +15,8 @@
  */
 package com.google.gct.testrecorder.settings;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.util.xmlb.XmlSerializerUtil;
@@ -82,7 +82,7 @@ public class TestRecorderSettings implements PersistentStateComponent<TestRecord
 
   @NotNull
   public static TestRecorderSettings getInstance() {
-    return ServiceManager.getService(TestRecorderSettings.class);
+    return ApplicationManager.getApplication().getService(TestRecorderSettings.class);
   }
 
   @Override
