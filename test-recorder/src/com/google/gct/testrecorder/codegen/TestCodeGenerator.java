409c64e92ed2350b5ca9ad87e46c48f9db6cbe27
Stas Negara <snegara@google.com>
2021-12-06 22:47:51 +0000

    Compute the package name of test class directly
    AndroidUtils.computePackageName is unreliable, because for some reason,
    it cannot find any source roots, so instead of relying on this API,
    establish the package name of the generated test class from its fully
    qualified class name.
    
    Fixes: 204529148
    Test: Unit + Manual
    Change-Id: I1dca2d12f0c981cff4022868280d0e2a4aca0b13

--- test-recorder/src/com/google/gct/testrecorder/codegen/TestCodeGenerator.java
+++ test-recorder/src/com/google/gct/testrecorder/codegen/TestCodeGenerator.java
@@ -17,8 +17,8 @@ package com.google.gct.testrecorder.codegen;
 
 import static com.google.gct.testrecorder.util.StringHelper.boxString;
 import static com.google.gct.testrecorder.util.StringHelper.getClassName;
+import static com.google.gct.testrecorder.util.StringHelper.getPackageName;
 import static com.google.gct.testrecorder.util.StringHelper.lowerCaseFirstCharacter;
-import static org.jetbrains.android.util.AndroidUtils.computePackageName;
 
 import com.android.annotations.VisibleForTesting;
 import com.android.tools.analytics.UsageTracker;
@@ -268,7 +268,7 @@ public class TestCodeGenerator {
     velocityContext.put("TestActivityName", getClassName(myLaunchedActivityName));
     velocityContext.put("ClassName", myTestClass.getName());
     velocityContext.put("TestMethodName", lowerCaseFirstCharacter(myTestClass.getName()));
-    velocityContext.put("PackageName", computePackageName(myTestClassModule, testCodeVirtualFile));
+    velocityContext.put("PackageName", getPackageName(myTestClass.getQualifiedName()));
     velocityContext.put("WasEverPaused", myWasEverPaused);
     velocityContext.put("ResourcePackageName", myResourcePackageName);
     velocityContext.put("EspressoPackageNamePrefix", myUsesAndroidxDependency ? "androidx" : "android.support");
