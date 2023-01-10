aa148a309a81ab4c43040c5247427c8da727df59
Vidish Naik <vidish@google.com>
2023-01-10 15:38:27 -0800

    Update device properties when device connects
    Update the device properties stateflow when the device connects.
    Add the connection port in the disambiguator field to be used in
    filtering firebase devices out from Physical devices tab.
    
    Bug: 243686058
    Test: Added
    Change-Id: I59d321fb1a89645c31f70291db0cd467980e5867

--- directaccess/src/com/google/gct/directaccess/provisioner/FirebaseDeviceProvisioner.kt
+++ directaccess/src/com/google/gct/directaccess/provisioner/FirebaseDeviceProvisioner.kt
@@ -243,6 +243,19 @@ class DirectAccessDeviceHandle(
         withContext(scope.coroutineContext) {
           stateFlow.update { Activating(it.properties) }
           connection.connect()
+          // Add disambiguator field that adds the port on which the device is connected to denote
+          // this is a firebase device.
+          // TODO(b/260153322): Remove once device manager moves to device provisioner framework
+          stateFlow.update {
+            Activating(
+              DirectAccessDeviceProperties.build {
+                manufacturer = it.properties.manufacturer
+                androidVersion = it.properties.androidVersion
+                model = it.properties.model
+                disambiguator = "${connection.port}"
+              }
+            )
+          }
         }
       }
 
@@ -283,7 +296,12 @@ class DirectAccessDeviceHandle(
       .syncPublisher(DeviceHeadsUpListener.TOPIC)
       .userInvolvementRequired(device.deviceInfoFlow.value.serialNumber, project)
     val properties = device.deviceProperties().allReadonly()
-    val deviceProperties = DirectAccessDeviceProperties.build { readCommonProperties(properties) }
+    val deviceProperties =
+      DirectAccessDeviceProperties.build {
+        readCommonProperties(properties)
+        // TODO(b/260153322): Remove once device manager moves to device provisioner framework
+        disambiguator = "${connection.port}"
+      }
     stateFlow.value = Connected(deviceProperties, device)
     device.invokeOnDisconnection { stateFlow.value = Disconnected(deviceProperties) }
     return true
--- directaccess/testSrc/com/google/gct/directaccess/provisioner/FirebaseDeviceProvisionerTest.kt
+++ directaccess/testSrc/com/google/gct/directaccess/provisioner/FirebaseDeviceProvisionerTest.kt
@@ -164,4 +164,23 @@ class FirebaseDeviceProvisionerTest {
     updateReservations(projectRule.project, plugin.templates)
     yieldUntil { provisioner.devices.value.isNotEmpty() }
   }
+
+  @Test
+  fun disambiguatorAddedAfterDeviceConnects() = runBlockingWithTimeout {
+    val template = plugin.templates.value[0]
+
+    // Activate device
+    template.activationAction.activate()
+    yieldUntil { provisioner.devices.value.isNotEmpty() }
+    assertThat(provisioner.devices.value.size).isEqualTo(1)
+
+    val handle = provisioner.devices.value[0]
+    assertThat(handle.state.properties.disambiguator).isNull()
+
+    handle.activationAction?.activate()
+    yieldUntil { handle.state.properties.disambiguator != null }
+
+    assertThat(handle.state).isInstanceOf(DirectAccessDeviceHandle.Activating::class.java)
+    assertThat(handle.state.properties.disambiguator).isEqualTo("12345")
+  }
 }
