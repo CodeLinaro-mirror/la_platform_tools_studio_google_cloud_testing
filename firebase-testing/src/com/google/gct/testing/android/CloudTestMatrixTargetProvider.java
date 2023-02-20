a6fc70bdfd0764200d80d4197cbf316577768259
Polina Koval <kovalp@google.com>
2023-02-20 15:17:02 +0000

    Throw exceptions instead of silently returning null
    Fixes: 269725699
    Test: manually run firebase tests
    Change-Id: I66366c3017d8c374f695b2d517d6631f880d8a2c

--- firebase-testing/src/com/google/gct/testing/android/CloudTestMatrixTargetProvider.java
+++ firebase-testing/src/com/google/gct/testing/android/CloudTestMatrixTargetProvider.java
@@ -29,7 +29,7 @@ import com.intellij.execution.ExecutionException;
 import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.executors.DefaultDebugExecutor;
+import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
@@ -84,7 +84,7 @@ public class CloudTestMatrixTargetProvider extends DeployTargetProvider {
     return new DeployTarget() {
       @Override
       public boolean hasCustomRunProfileState(@NotNull Executor executor) {
-        return !(executor instanceof DefaultDebugExecutor);
+        return DefaultRunExecutor.EXECUTOR_ID.equals(executor.getId());
       }
 
       @Override
@@ -92,9 +92,8 @@ public class CloudTestMatrixTargetProvider extends DeployTargetProvider {
                                                 @NotNull ExecutionEnvironment env,
                                                 @NotNull DeployTargetState state) throws ExecutionException {
         RunProfile runProfile = env.getRunProfile();
-        // It is expected to be invoked for test run configurations only.
         if (!(runProfile instanceof AndroidTestRunConfiguration)) {
-          return null;
+          throw new ExecutionException("CloudMatrixTestRunningState is expected to be invoked for test run configurations only.");
         }
 
         AndroidTestRunConfiguration runConfiguration = (AndroidTestRunConfiguration)runProfile;
