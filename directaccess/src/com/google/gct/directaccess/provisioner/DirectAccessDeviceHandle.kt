7a562356e94592fbd54924d4b465a8cec9887789
James Rose <rosej@google.com>
2023-06-13 11:00:02 -0700

    Replace invokeOnDisconnection with awaitTermination
    See change Ie4297f96ac560 for details.
    
    Bug: 275572248
    Test: LeakChecker
    Change-Id: Icee9811a6993eeb013a488651de4be8830c838fb

--- directaccess/src/com/google/gct/directaccess/provisioner/DirectAccessDeviceHandle.kt
+++ directaccess/src/com/google/gct/directaccess/provisioner/DirectAccessDeviceHandle.kt
@@ -29,7 +29,7 @@ import com.android.sdklib.deviceprovisioner.ReservationAction
 import com.android.sdklib.deviceprovisioner.ReservationState
 import com.android.sdklib.deviceprovisioner.Resolution
 import com.android.sdklib.deviceprovisioner.asMap
-import com.android.sdklib.deviceprovisioner.invokeOnDisconnection
+import com.android.sdklib.deviceprovisioner.awaitDisconnection
 import com.android.tools.adbbridge.Reservation
 import com.android.tools.idea.devicemanager.DeviceType
 import com.android.tools.idea.run.DeviceHeadsUpListener
@@ -356,7 +356,8 @@ class DirectAccessDeviceHandle(
     val properties = device.deviceProperties().all().asMap()
     val deviceProperties = DirectAccessDeviceProperties.build { readCommonProperties(properties) }
     stateFlow.update { DeviceState.Connected(deviceProperties, device, it.reservation) }
-    device.invokeOnDisconnection {
+    scope.launch {
+      device.awaitDisconnection()
       if (!hasUserDisconnectedDevice) {
         notificationManager.showDeviceDisconnectedNotification(
           state.reservation?.endTime?.epochSecond
