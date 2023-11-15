c3b8b08ec9b4646121d7171f60905c67a428a5f7
Jerome Gaillard <jgaillard@google.com>
2023-11-15 16:38:19 +0000

    Revert "Track reservation end with correct state"
    This reverts commit 6545ee49195bc87b5b6c9e398cecca14e2b77a61.
    
    Reason for revert: Studio build broken
    
    Change-Id: Id42af910372fd1fcabd347f8482f46d5529d3266

--- directaccess/src/com/google/gct/directaccess/provisioner/DirectAccessDeviceHandle.kt
+++ directaccess/src/com/google/gct/directaccess/provisioner/DirectAccessDeviceHandle.kt
@@ -130,33 +130,33 @@ class DirectAccessDeviceHandle(
     scope
       .launch { reservationFlow.takeWhile { !it.sessionState.isClosed() }.collect() }
       .invokeOnCompletion { throwable ->
-        val sessionState = reservationFlow.value.sessionState
-        if (!shouldTrackEndReservation(throwable, sessionState)) {
+        if (!shouldTrackEndReservation(throwable, reservationFlow.value.sessionState)) {
           return@invokeOnCompletion
         }
         if (hasReservationActivated && state.connectedDevice != null) {
           notificationManager.showReservationExpiredNotification()
         }
 
-        when (sessionState) {
-          SessionState.EXPIRED -> trackEndReservation(true, EndReservationType.EXPIRE)
-          SessionState.FINISHED -> trackEndReservation(true, EndReservationType.FORCE_CHECK_IN)
-          else ->
-            trackEndReservation(false, EndReservationType.ERROR, FailureReason.UNKNOWN_FAILURE)
+        if (reservationFlow.value.sessionState == SessionState.EXPIRED) {
+          trackEndReservation(true, EndReservationType.EXPIRE)
+        } else {
+          trackEndReservation(false, EndReservationType.ERROR, FailureReason.UNKNOWN_FAILURE)
         }
       }
   }
 
   /**
-   * End reservation should be tracked when the session is closed and the throwable is null or
-   * [CancellationException] It should not be tracked if the session is not closed.
+   * End reservation should not be tracked if:
+   * 1. User has not force checked in the device since it is already tracked
+   * 2. The reservation state is still REQUESTED, PENDING or ACTIVE
+   *
+   * End reservation should be tracked when:
+   * 1. The throwable is null
+   * 2. If the throwable is [CancellationException]
    */
   private fun shouldTrackEndReservation(throwable: Throwable?, sessionState: SessionState) =
-    if (sessionState.isClosed()) {
-      throwable?.let { it is CancellationException } ?: true
-    } else {
-      false
-    }
+    if (hasUserForceCheckedInDevice || !sessionState.isClosed()) false
+    else throwable?.let { it is CancellationException } ?: true
 
   /** Map Reservation to its device provisioner format. */
   private fun mapReservation(
@@ -358,6 +358,7 @@ class DirectAccessDeviceHandle(
             )
             throw DeviceActionException("Could not end reservation", e)
           }
+          trackEndReservation(true, EndReservationType.FORCE_CHECK_IN)
           service<DirectAccessFeatureSurveys>().trackDisconnection()
         }
 
