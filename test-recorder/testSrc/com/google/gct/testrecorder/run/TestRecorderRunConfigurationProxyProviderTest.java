f0141a625448f636fb28a2543ebab700df7a55ac
Andy Magee <andymagee@google.com>
2024-02-16 09:12:34 -0800

    Remove deprecated PlatformTestCase
    PlatformTestCase is deprecated, and can be replaced directly with its
    super class HeavyPlatformTestCase.
    
    Bug: n/a
    Test: n/a
    Change-Id: I093c979787e79b1b4ddccf8174e7e26de459cb56

--- test-recorder/testSrc/com/google/gct/testrecorder/run/TestRecorderRunConfigurationProxyProviderTest.java
+++ test-recorder/testSrc/com/google/gct/testrecorder/run/TestRecorderRunConfigurationProxyProviderTest.java
@@ -17,9 +17,9 @@ package com.google.gct.testrecorder.run;
 
 import com.android.tools.idea.run.AndroidRunConfigurationType;
 import com.intellij.execution.configurations.RunConfiguration;
-import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.HeavyPlatformTestCase;
 
-public class TestRecorderRunConfigurationProxyProviderTest extends PlatformTestCase {
+public class TestRecorderRunConfigurationProxyProviderTest extends HeavyPlatformTestCase {
 
   public void testTestRecorderAndroidRunConfigurationProxy() {
     assertTrue("Too few Test Recorder run configuration providers", TestRecorderRunConfigurationProxy.EP_NAME.getExtensions().length > 0);
