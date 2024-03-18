5223c3572fdc47c0ba03fa361bea176653b7d794
Kainan Wang <knwang@google.com>
2024-03-18 11:54:46 -0700

    Add Learn more link for warning dialog realted to blaze plan
    Bug: 329856634
    Test: updated
    Change-Id: I0630aebd4afe5dee188a5321a7b5bd2dea4da44b
    (cherry picked from commit 231573fb64e5623813c36361982888ee6574fcd7)

--- directaccess/testSrc/com/google/gct/directaccess/provisioner/DirectAccessDeviceProvisionerTestWithLogin2.kt
+++ directaccess/testSrc/com/google/gct/directaccess/provisioner/DirectAccessDeviceProvisionerTestWithLogin2.kt
@@ -1298,7 +1298,7 @@ class DirectAccessDeviceProvisionerTestWithLogin2 {
         doAnswer { billingEnabledFlow }.whenever(cloudProjectManager).isBillingEnabledFlow
 
         TestDialogManager.setTestDialog { message ->
-          assertThat(message).isEqualTo(singleMessage)
+          assertThat(message).startsWith(singleMessage)
           countDownLatch.countDown()
           Messages.YES
         }
@@ -1307,7 +1307,7 @@ class DirectAccessDeviceProvisionerTestWithLogin2 {
         yieldUntil { plugin.devices.value.size == 1 }
 
         TestDialogManager.setTestDialog { message ->
-          assertThat(message).isEqualTo(multiMessage)
+          assertThat(message).startsWith(multiMessage)
           countDownLatch.countDown()
           Messages.YES
         }
