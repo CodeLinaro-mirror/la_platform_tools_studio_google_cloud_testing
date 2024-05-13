bc8807f1c4fcbc07306c8db4c87df79a99b19b39
Joe Baker-Malone <jbakermalone@google.com>
2024-05-13 12:07:46 -0700

    Decrease permissions check frequency
    Studio is being rate-limited on the permissions check api after stable
    release. We're trying to increase the limit, but should also decrease
    the request freqency.
    
    Test: none
    Bug: 340253761
    Change-Id: I757f198992e2b585674ac1a1b30c31fcd417e3c1
    (cherry picked from commit 03fe2f0e7e50d9cb0cede989986307034f7e249f)

--- directaccess/src/com/google/gct/directaccess/DirectAccessCloudProjectManager.kt
+++ directaccess/src/com/google/gct/directaccess/DirectAccessCloudProjectManager.kt
@@ -115,7 +115,7 @@ class DirectAccessCloudProjectManager(
     }
 
   val permissionFlow: RefreshableStateFlow<DirectAccessPermissionStatus> =
-    RefreshableStateFlow(scope, TimeUnit.MINUTES.toMillis(2)) {
+    RefreshableStateFlow(scope, TimeUnit.MINUTES.toMillis(5)) {
       checkDirectAccessPermission(cloudProject)
     }
 
