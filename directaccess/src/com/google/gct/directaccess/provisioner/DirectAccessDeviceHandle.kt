537f24a117ab0531669ec6a36687eff433c228b8
Kainan Wang <knwang@google.com>
2024-07-02 11:33:24 -0700

    Report detailed information in dialog when extend reservation failed
    Fixes: 340947927
    Test: DirectAccessDeviceProvisionerTest
    Change-Id: I310f45ead5ebca7b7b410c23f97e6d524a6513be

--- directaccess/src/com/google/gct/directaccess/provisioner/DirectAccessDeviceHandle.kt
+++ directaccess/src/com/google/gct/directaccess/provisioner/DirectAccessDeviceHandle.kt
@@ -33,6 +33,8 @@ import com.android.sdklib.deviceprovisioner.asMap
 import com.android.sdklib.deviceprovisioner.awaitDisconnection
 import com.android.tools.adbbridge.Reservation
 import com.android.tools.adbbridge.Reservation.SessionState
+import com.android.tools.idea.io.grpc.Status
+import com.android.tools.idea.io.grpc.StatusRuntimeException
 import com.android.tools.idea.run.DeviceHeadsUpListener
 import com.android.tools.idea.streaming.RUNNING_DEVICES_TOOL_WINDOW_ID
 import com.android.tools.idea.streaming.core.StreamingDevicePanel
@@ -393,6 +395,16 @@ class DirectAccessDeviceHandle(
         } catch (e: CancellationException) {
           trackExtendReservation(false, duration, FailureReason.UNKNOWN_FAILURE)
           throw e
+        } catch (e: StatusRuntimeException) {
+          trackExtendReservation(false, duration, FailureReason.UNKNOWN_FAILURE)
+          if (e.status.code == Status.Code.RESOURCE_EXHAUSTED) {
+            throw DeviceActionException(
+              "All Spark plan minutes for the current period have been used. " +
+                "Upgrade to a Blaze plan to immediately continue using this service.",
+              e,
+            )
+          }
+          throw DeviceActionException("Failed to extend reservation. Please try again.", e)
         } catch (e: Exception) {
           trackExtendReservation(false, duration, FailureReason.UNKNOWN_FAILURE)
           throw DeviceActionException("Failed to extend reservation. Please try again.", e)
