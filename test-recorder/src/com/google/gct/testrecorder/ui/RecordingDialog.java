1fb43927a70327018d00391f60a6cb995d8d3668
Venkatesh D <venkicodes@google.com>
2026-06-30 11:01:46 +0000

    Secure element picker in Test Recorder assertion dialog
    Ensures that element identifiers (resource IDs and class names)
    sourced from the device's UI Automator dump are XML-entity escaped
    before being rendered in the assertion element picker combo box.
    
    This prevents Swing's BasicHTML engine from executing malicious
    markup contained within these strings, mitigating SSRF and
    information leak risks.
    
    Bug: 509658806
    Test: Existing
    Change-Id: Ic433da8033ed4d27424a1757ba087b4ea55bb5a3

--- test-recorder/src/com/google/gct/testrecorder/ui/RecordingDialog.java
+++ test-recorder/src/com/google/gct/testrecorder/ui/RecordingDialog.java
@@ -66,6 +66,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.xml.util.XmlStringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileWrapper;
 import com.intellij.psi.PsiDirectory;
@@ -302,7 +303,7 @@ public class RecordingDialog extends DialogWrapper implements TestRecorderEventL
           }
           String resourceId = getResourceId(node);
           String nodeString = resourceId.isEmpty() ? getClassName(node) : resourceId;
-          return super.getListCellRendererComponent(list, prefix + nodeString, index, isSelected, cellHasFocus);
+          return super.getListCellRendererComponent(list, prefix + XmlStringUtil.escapeString(nodeString), index, isSelected, cellHasFocus);
         }
         else {
           // non UI element
