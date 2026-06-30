d970799d1eede3b492233b2fe67c3efc32ae5e17
Venkatesh D <venkicodes@google.com>
2026-06-30 11:00:05 +0000

    Escape replacement and asserted text in Test Recorder UI
    Secures the main event list renderer by replacing incorrect
    Java-literal escaping with proper XML entity escaping for
    untrusted, device-originated strings.
    
    The previous use of StringUtil.escapeStringCharacters was insufficient
    as it targets string literal characters (like quotes and backslashes)
    but leaves HTML tags like '<' and '>' untouched, allowing for
    potential SSRF via Swing HTML rendering.
    
    Bug: 509658806
    Test: Existing
    Change-Id: I3cb78b85efc5dc8fd18b86ae5c964e26802a3b62

--- test-recorder/src/com/google/gct/testrecorder/ui/TestRecorderListRenderer.java
+++ test-recorder/src/com/google/gct/testrecorder/ui/TestRecorderListRenderer.java
@@ -18,6 +18,7 @@ package com.google.gct.testrecorder.ui;
 import com.google.gct.testrecorder.event.TestRecorderAssertion;
 import com.google.gct.testrecorder.event.TestRecorderEvent;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.xml.util.XmlStringUtil;
 
 import com.intellij.ui.JBColor;
 import javax.swing.*;
@@ -47,7 +48,7 @@ public class TestRecorderListRenderer extends DefaultListCellRenderer {
       TestRecorderEvent event = (TestRecorderEvent) value;
       if (event.isTextChange()) {
         renderString = String.format(TEXT_CHANGE_EVENT_FORMAT, outerDivTextColor, event.getRendererString(),
-                                     StringUtil.escapeStringCharacters(event.getReplacementText()));
+                                     XmlStringUtil.escapeString(event.getReplacementText()));
       } else if (event.isPressEvent()) {
         renderString = String.format(PRESS_EVENT_FORMAT, outerDivTextColor, event.getRendererString());
       } else if (event.isViewLongClick()) {
@@ -57,8 +58,7 @@ public class TestRecorderListRenderer extends DefaultListCellRenderer {
       } else if (event.isDelayedMessagePost()) {
         renderString = String.format(DELAYED_MESSAGE_POST_EVENT_FORMAT, outerDivTextColor, event.getRendererString());
       } else if (event.isPermissionsRequest()) {
-        renderString = String.format(PERMISSIONS_REQUEST_EVENT_FORMAT, outerDivTextColor,
-                                     StringUtil.escapeStringCharacters(event.getRendererString()));
+        renderString = String.format(PERMISSIONS_REQUEST_EVENT_FORMAT, outerDivTextColor, event.getRendererString());
       } else {
         // Click event.
         renderString = String.format(CLICK_EVENT_FORMAT, outerDivTextColor, event.getRendererString());
@@ -68,10 +68,10 @@ public class TestRecorderListRenderer extends DefaultListCellRenderer {
       TestRecorderAssertion assertion = (TestRecorderAssertion) value;
       String assertedText = assertion.getText();
       if (assertedText == null) {
-        renderString = String.format(ASSERTION_FORMAT_ONE_LINE, outerDivTextColor, assertion.getRendererString(), assertion.getRule());
+        renderString = String.format(ASSERTION_FORMAT_ONE_LINE, outerDivTextColor, assertion.getRendererString(), XmlStringUtil.escapeString(assertion.getRule()));
       } else {
-        renderString = String.format(ASSERTION_FORMAT_MULTI_LINE, outerDivTextColor, assertion.getRendererString(), assertion.getRule(),
-                                     StringUtil.escapeStringCharacters(assertedText));
+        renderString = String.format(ASSERTION_FORMAT_MULTI_LINE, outerDivTextColor, assertion.getRendererString(), XmlStringUtil.escapeString(assertion.getRule()),
+                                     XmlStringUtil.escapeString(assertedText));
       }
     } else {
       throw new RuntimeException("Unsupported Test Recorder entity: " + value.toString());
